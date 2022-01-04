84e609dd0f20b1bb9b6ac7c0c91a2b607403fce8
Dmitriy <41371080+azarro777@users.noreply.github.com>
2022-01-04 11:41:46 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<PeoplePage/>} />
+        <Route exact path='/star-wars' element={<PeoplePage/>} />
         <Route path='/star-wars/planets' element={<PlanetsPage/>} />
         <Route/>
         <Route path='/star-wars/starships' element={<StarshipsPage/>} />
